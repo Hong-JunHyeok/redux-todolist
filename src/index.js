d882d0f23de60ffec28ca1077cf670710a38c62b
HongJunHyeok <edb1631@naver.com>
2020-11-30 17:20:48 +0900

    COMPLETE

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { useLayoutEffect } from "react";
+import { createStore } from "redux";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const input = document.querySelector("input");
+const submit = document.querySelector("button");
+const ul = document.querySelector("ul");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const ADD = "ADD";
+const REMOVE = "REMOVE";
+
+const todoReducer = (state = [], action) => {
+    switch (action.type) {
+        case ADD:
+            return [{ text: action.text, id: Date.now() }, ...state];
+        case REMOVE:
+            return state.filter((todo) => todo.id !== action.id);
+        default:
+            return state;
+    }
+};
+
+const todoStore = createStore(todoReducer);
+
+todoStore.subscribe(() => console.log(todoStore.getState()));
+
+const paintTodo = () => {
+    const todos = todoStore.getState();
+    ul.innerHTML = "";
+    todos.forEach((todo) => {
+        const li = document.createElement("li");
+        const btn = document.createElement("button");
+        li.id = todo.id;
+        li.innerText = todo.text;
+        btn.innerText = "DEL";
+        btn.addEventListener("click", deleteTodo);
+        ul.appendChild(li);
+        li.appendChild(btn);
+    });
+};
+todoStore.subscribe(paintTodo);
+
+const addTodo = (text) => {
+    todoStore.dispatch({ type: ADD, text });
+};
+
+const deleteTodo = (e) => {
+    const id = parseInt(e.target.parentNode.id);
+    todoStore.dispatch({ type: REMOVE, id });
+};
+
+const onSubmit = (e) => {
+    e.preventDefault();
+    const todo = input.value;
+    addTodo(todo);
+    input.value = "";
+};
+
+submit.addEventListener("click", onSubmit);
